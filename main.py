e1e79cdc84c92ff80a2d71149686a3210b4556c9
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-04 11:01:11 -0700

    Add player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 import pygame
 
 def main():
@@ -11,12 +12,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
 
         # Cap the frame rate at 60 FPS and calculate delta time
