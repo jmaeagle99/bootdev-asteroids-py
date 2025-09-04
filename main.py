c01947996444a24d8f1eee942790876026c7fc82
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-04 10:52:54 -0700

    Set frame rate

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +18,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # Cap the frame rate at 60 FPS and calculate delta time
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
